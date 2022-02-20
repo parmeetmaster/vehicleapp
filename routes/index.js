7e551f4bf4dd32293094cd4ca396222aa948351a
parmeet <parmeets834@gmail.com>
2022-02-20 17:34:25 +0530

    assa

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    var con = mysql.createConnection({
+        host     : '52.66.168.190',
+        port     :'3306',
+        user     : 'root',
+        password : '12345678',
+        database : 'mysql'
+    });
 
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
     res.render('index', {
         title: 'Express',
     });
