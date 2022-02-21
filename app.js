ec5821fad8b901d42a8a1c69f909cda69b4a8b53
parmeet <parmeets834@gmail.com>
2022-02-21 22:44:49 +0530

    assa

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(express.urlencoded())
 app.use(express.urlencoded({
   extended: true
 }))
